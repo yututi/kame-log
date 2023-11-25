ae97f5519f9aed2b91165423b5ea37c6aa1a85d2
yututi <y.tsuchiya0207@gmail.com>
2023-11-25 20:52:47 +0900

    gcpの認証設定追加

--- web/src/services/gc/storage.ts
+++ web/src/services/gc/storage.ts
@@ -1,6 +1,13 @@
-import { Storage } from "@google-cloud/storage"
-
-const storage = new Storage()
+import { Storage } from '@google-cloud/storage'
+let credentials
+if (process.env.GOOGLE_SERVICE_KEY) {
+  credentials = JSON.parse(
+    Buffer.from(process.env.GOOGLE_SERVICE_KEY, 'base64').toString().replace(/\n/g, ''),
+  )
+}
+const storage = new Storage({
+  credentials,
+})
 
 export const get = async (bucketName: string, path: string) => {
   const bucket = storage.bucket(bucketName)
@@ -9,6 +16,6 @@ export const get = async (bucketName: string, path: string) => {
   if (!exists) return null
 
   return await file.download({
-    decompress: file.metadata.contentEncoding === "gzip",
+    decompress: file.metadata.contentEncoding === 'gzip',
   })
 }

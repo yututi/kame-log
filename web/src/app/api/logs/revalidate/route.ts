486c2d4e0ab028a871993dd4d8e014275b562587
yututi <y.tsuchiya0207@gmail.com>
2024-03-04 01:20:55 +0900

    update route

--- web/src/app/api/logs/revalidate/route.ts
+++ web/src/app/api/logs/revalidate/route.ts
@@ -1,14 +1,15 @@
 import { revalidatePath } from "next/cache"
+import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   const secret = new URL(request.url).searchParams.get("secret")
   if (secret !== process.env.MY_SECRET_TOKEN) {
-    return Response.json({ message: "Invalid secret" }, { status: 401 })
+    return NextResponse.json({ message: "Invalid secret" }, { status: 401 })
   }
 
   const { date } = await request.json()
   revalidatePath(`/logs/${date}`)
   revalidatePath(`/logs`)
 
-  return Response.json({})
+  return NextResponse.json({})
 }

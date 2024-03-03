f3fe08cb9cd5a5cd67e12d6f6b3d8f41e5f40ea5
yututi <y.tsuchiya0207@gmail.com>
2024-03-04 01:31:42 +0900

    enable timezone

--- web/src/app/logs/[date]/page.tsx
+++ web/src/app/logs/[date]/page.tsx
@@ -6,7 +6,7 @@ import dayjs from "@/utils/timezonedDayjs"
 export const revalidate = 60
 
 export default async function Page({ params: { date } }: { params: { date: string } }) {
-  const logs = await fetchLogs(dayjs(date))
+  const logs = await fetchLogs(dayjs(date).tz("Asia/Tokyo"))
   return <Template date={date} logs={logs}></Template>
 }
 
